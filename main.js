509024740af853680a96ce8eb63a8b79769e2300
boedegoat <bhremada.fka@gmail.com>
2021-06-12 17:53:51 +0700

    create mobile version and modifying the js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const shareBtn = document.querySelector('.card-share-btn')
+const shareWrapper = shareBtn.parentNode
 
 document.addEventListener('click', (e) => {
-  if (shareBtn.contains(e.target)) {
-    return shareBtn.parentNode.classList.toggle('active')
+  if (e.target === shareBtn) {
+    shareWrapper.classList.toggle('active')
+  } else if (shareWrapper.className.includes('active') && !shareWrapper.contains(e.target)) {
+    shareBtn.parentNode.classList.remove('active')
   }
-  shareBtn.parentNode.classList.remove('active')
 })
