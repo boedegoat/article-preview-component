594a0012e5adc87ab4b6bff75ec7034aeadac8d9
boedegoat <bhremada.fka@gmail.com>
2021-06-13 12:38:29 +0700

    filled aria attr to share btn

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const shareWrapper = shareBtn.parentNode
 document.addEventListener('click', (e) => {
   if (e.target === shareBtn) {
     shareWrapper.classList.toggle('active')
+    if (shareBtn.getAttribute('aria-expanded') === 'false') {
+      shareBtn.setAttribute('aria-expanded', 'true')
+    } else {
+      shareBtn.setAttribute('aria-expanded', 'false')
+    }
   } else if (shareWrapper.className.includes('active') && !shareWrapper.contains(e.target)) {
     shareBtn.parentNode.classList.remove('active')
+    shareBtn.setAttribute('aria-expanded', 'false')
   }
 })
